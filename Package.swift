65c85d8edb732b437df8979eb7c337b5e5e6e356
Grigory Entin <grigory.entin@gmail.com>
2021-02-19 10:53:38 +0100

    Added executable product.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "MountMonitor",
     platforms: [.macOS(.v11)],
+    products: [
+        .executable(name: "MountMonitor", targets: ["MountMonitor"]),
+    ],
     targets: [
         .target(
             name: "MountMonitor",
